ca80b2a9a8332151a8902cb4f4f3fd070efb1062
Jyoyelchuri <45900970+Jyoyelchuri@users.noreply.github.com>
2020-01-03 12:44:43 +0530

    Delete sequence.py

--- sequence.py
+++
@@ -1,9 +0,0 @@
-CREATE SEQUENCE public.base_transaction_seqnum_seq
-    INCREMENT 1
-    START 1
-    MINVALUE 1
-    MAXVALUE 2147483647
-    CACHE 1;
-
-ALTER SEQUENCE public.base_transaction_seqnum_seq
-    OWNER TO postgres;
